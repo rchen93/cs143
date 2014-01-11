de813fc7d1de0e6f1b0b17a44254930ccdffd649
rchen93 <rchen93@ucla.edu>
2014-01-11 15:13:04 -0800

    Edit calculator.php
    Added decimal functionality and zero division check

--- calculator.php
+++ calculator.php
@@ -12,15 +12,37 @@
 </form>
 
 <?php
-// TO DO: DON'T DIVIDE BY ZERO
+// DECIMAL POINTS
 $expr = $_GET["expr"]; //expression to be matched
-$pattern = '/^[+ -]?[0-9]+(((\+\-)|(\-\+)|(\*\-)|(\*\+)|(\/\+)|(\/\-)|[\/\+\*\-])[0-9]+)*$/'; // pattern to match against
+$pattern = '/^([+ -]?[0-9]+\.?[0-9]*)(((\+\-)|(\-\+)|(\*\-)|(\*\+)|(\/\+)|(\/\-)|[\/\+\*\-])[0-9]+\.?[0-9]*)*$/'; // pattern to match against
 preg_match($pattern, $expr, $matches); 
+
+$zero = FALSE;
+
+function e($errno, $errstr, $errfile, $errline) {
+    if ($errstr == "Division by zero") {
+        global $zero;
+        $zero = TRUE;
+        //echo $errstr."<br />";
+    }
+}
+
+set_error_handler('e');
+
 if (!empty($expr) && preg_match($pattern, $expr, $matches))
 {
-	eval("\$ans = $expr;"); // eval is weird
 
-	echo "Answer: ". $ans;
+	eval("\$ans = $expr;"); 
+
+	if ($zero == TRUE) 
+	{ 
+    	echo "Error: Division by Zero <br />";
+	} 
+	else 
+	{
+		echo "Answer: ". $ans;
+	}
+
 }
 else if (!empty($expr))
 {
