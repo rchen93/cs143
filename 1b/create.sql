b0bbdac8a7b5c6d26272d040967628ebf312581e
rchen93 <rchen93@ucla.edu>
2014-01-28 15:57:04 -0800

    Removed Primary Keys from some of the tables
    Edit: MovieGenre, MovieDirector, MovieActor, Review tables

--- 1b/create.sql
+++ 1b/create.sql
@@ -1,9 +1,9 @@
 CREATE TABLE Movie (id int, title varchar(100), year int, rating varchar(10), company varchar(50), PRIMARY KEY(id));
 CREATE TABLE Actor (id int, last varchar(20), first varchar(20), sex varchar(6), dob date, dod date, PRIMARY KEY(id));
 CREATE TABLE Director (id int, last varchar(20), first varchar(20), dob date, dod date, PRIMARY KEY(id));
-CREATE TABLE MovieGenre (mid int, genre varchar(20), PRIMARY KEY(mid));
-CREATE TABLE MovieDirector (mid int, did int, PRIMARY KEY(mid));
-CREATE TABLE MovieActor (mid int, aid int, role varchar(50), PRIMARY KEY(mid));
-CREATE TABLE Review (name varchar(20), time timestamp, mid int, rating int, comment varchar(500), PRIMARY KEY(name));
+CREATE TABLE MovieGenre (mid int, genre varchar(20));
+CREATE TABLE MovieDirector (mid int, did int);
+CREATE TABLE MovieActor (mid int, aid int, role varchar(50));
+CREATE TABLE Review (name varchar(20), time timestamp, mid int, rating int, comment varchar(500));
 CREATE TABLE MaxPersonID (id int, PRIMARY KEY(id));
 CREATE TABLE MaxMovieID (id int, PRIMARY KEY(id));
